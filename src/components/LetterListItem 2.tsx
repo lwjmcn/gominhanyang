72a7d38046bf8dbc3942cc060df2e6bb05489975
Sunmin-Lim <imsnmn24@gmail.com>
2025-05-17 18:51:12 +0900

    merge changes

---
+++ src/components/LetterListItem 2.tsx
@@ -0,0 +1,27 @@
+import { EmotionType } from '@/lib/type/letter.type';
+import styles from './letterlistitem.module.css';
+
+interface Letter {
+  _id: string;
+  title: string;
+  emotion: EmotionType;
+  created_at: string;
+}
+interface LetterListItemProps {
+  letter: Letter;
+  onClick: () => void;
+}
+
+export default function LetterListItem({ letter, onClick }: LetterListItemProps) {
+  return (
+    <div className={styles.container} onClick={onClick}>
+      <div>
+        <h3 className={styles.title}>{letter.title}</h3>
+      </div>
+      <div>
+        <p className={styles.date}>{new Date(letter.created_at).toLocaleDateString()}</p>
+        <span className={styles.emotion}>{letter.emotion}</span>
+      </div>
+    </div>
+  );
+}
